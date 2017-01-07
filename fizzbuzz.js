be92d40c6695ab0232f8fb74feff46c02d536f43
Temmyogunbo <Ogunbotemilola@yahoo.com>
2017-01-07 12:08:36 +0100

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,9 @@
 function fizzBuzz(n){
   if(n% 3=== 0 && n% 5===0){ // n divides 3 and 5
-    return "fizzBuzz"
+    return "FizzBuzz"
   }
   else if(n % 3 === 0){     //n divides 3 only
-    return "fizz"
+    return "Fizz"
   }
   else if(n % 5 === 0){   //n divides 5 only
     return "Buzz"
